faaec705d08621a9bc90b922f5ac3b409d10d733
shimanto <s.m.sajjadhossanshimanto.c@gmail.com>
2022-09-01 02:12:02 +0000

    player for vimeo

--- p.py
+++ p.py
@@ -2,12 +2,12 @@
 import re
 import requests
 import json
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
 
 #%%
-url = "https://vimeo.com/api/oembed.json?url=https%3A%2F%2Fvimeo.com%2F622338938"
-url = "https://vimeo.com/api/oembed.json?"
+# url = "https://vimeo.com/api/oembed.json?url=https%3A%2F%2Fvimeo.com%2F622338938"
+# url = "https://vimeo.com/api/oembed.json?"
 
 vimeo_id = 622338938
 
@@ -20,23 +20,18 @@ headers = {
   'Accept-Language': 'en-US,en;q=0.9',
 }
 
-print(response.json())
-
-class EduHive:
-    def __init__(self) -> None:
-        pass
-
-    # def 
 
 class Vimeo:
-    embed_url = "https://vimeo.com/api/oembed.json?"
+    embed_url = "https://vimeo.com/api/oembed.json"
+    # TODO: we can also save h tag and then no need to fetch from embed link
 
     def __init__(self, vimeo_id) -> None:
         self.vimeo_id = vimeo_id
+        self.content = None
 
     def embed_link(self):
         payload={
-            "url": f"https://vimeo.com/{video_id}"
+            "url": f"https://vimeo.com/{self.vimeo_id}"
         }
         r = requests.request("GET", self.embed_url, headers=headers, params=payload)
         
@@ -47,5 +42,25 @@ class Vimeo:
         return url
 
     def player(self):
+        url = self.embed_link()
+        r = requests.request("GET", url, headers=headers)
+        r = re.search(
+            r" var config = (.+?); if \(!config",
+            r.text
+        ).group(1)
+        
+        config = json.loads(r)
+        self.content = {
+            i['quality']:i
+            for i in config['request']["files"]['progressive']
+        }
+        return self.content
+
+    def download(self, quality):
         pass
+
+
+# p=Vimeo(vimeo_id)
+# print(p.embed_link())
+
 
